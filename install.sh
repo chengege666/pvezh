94acd48b2e25af3fd1cfafc6721cc7f0b0ace7d0
chengege666 <609906658@qq.com>
2026-06-19 16:17:34 +0800

    1

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# PVE 镜像转换工具 - 一键安装
-REPO="chengege666/pvezh"
-VERSION=$(curl -s https://api.github.com/repos/${REPO}/releases/latest | grep -o '"tag_name": "[^"]*' | cut -d'"' -f4 || echo "v1.0")
-wget -qO /tmp/pvezh "https://github.com/${REPO}/releases/download/${VERSION}/pvezh"
-chmod +x /tmp/pvezh
-/tmp/pvezh
